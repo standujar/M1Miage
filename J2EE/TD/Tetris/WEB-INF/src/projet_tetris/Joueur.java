c46c7cba3d94e8d229d16cb1023e29959b07391f
Stan <sandujar@free.fr>
2016-09-27 21:10:16 +0200

    Update - Tetris
    Maj Tetris Servlet Online

---
+++ J2EE/TD/Tetris/WEB-INF/src/projet_tetris/Joueur.java
@@ -0,0 +1,52 @@
+package projet_tetris;
+
+import java.io.Serializable;
+
+public class Joueur implements Serializable {
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+private String nom;
+  private int niveau;
+  private int score;
+  
+  
+  public Joueur(String nom, int score, int niveau)
+  {
+	  this.nom = nom;
+	  this.niveau = niveau;
+	  this.score = score;
+	    
+  }
+
+  
+	public String getNom() {
+		return nom;
+	}
+	
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+	public int getNiveau() {
+		return niveau;
+	}
+	
+	
+	public void setNiveau(int niveau) {
+		this.niveau = niveau;
+	}
+	
+	
+	public int getScore() {
+		return score;
+	}
+	
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
+}
